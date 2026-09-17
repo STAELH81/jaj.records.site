925b1328ea7b8ef78d6b4002850fd40dccee5a44
Cha <zambiasisacha@gmail.com>
2026-09-17 16:16:08 +0200

    chore: load Phase 6B final polish

--- js/visualizer.js
+++ js/visualizer.js
@@ -2,6 +2,7 @@
 // SVG avoids the hidden-view canvas sizing bug that could leave Visualisations black.
 import './visualizer-svg.js';
 import './visualizer-layout-fix.js';
+import './player-phase6b-final.js';
 
 // The Skin Chooser hotfix is another sibling import of cloud-sync.js.
 // Load the theme upgrade on the next task so that its sidebar/button wiring already exists.
